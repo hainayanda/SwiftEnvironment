512d56e4996a435a3e47039a918a933570eb08d6
Nayanda Haberty <hainayanda@outlook.com>
2024-03-15 23:54:56 +0700

    Initialize the project

---
+++ Package.swift
@@ -0,0 +1,45 @@
+// swift-tools-version:5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+import CompilerPluginSupport
+
+let package = Package(
+    name: "SwiftEnvironment",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
+    products: [
+        .library(
+            name: "SwiftEnvironment",
+            targets: ["SwiftEnvironment"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
+        .package(url: "https://github.com/hainayanda/Chary.git", .upToNextMajor(from: "1.0.7"))
+    ],
+    targets: [
+        .target(
+            name: "SwiftEnvironment",
+            dependencies: ["SwiftEnvironmentMacro", "Chary"]
+        ),
+        .macro(
+            name: "SwiftEnvironmentMacro",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ]
+        ),
+        .testTarget(
+            name: "SwiftEnvironmentTests",
+            dependencies: [
+                "SwiftEnvironment", "SwiftEnvironmentMacro",
+                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
+            ]
+        ),
+    ]
+)
---
+++ Sources/SwiftEnvironment/Typealiases.swift
@@ -0,0 +1,12 @@
+//
+//  TypeAliases.swift
+//  
+//
+//  Created by Nayanda Haberty on 15/3/24.
+//
+
+import Foundation
+import SwiftUI
+
+public typealias EnvironmentValues = SwiftUI.EnvironmentValues
+public typealias EnvironmentKey = SwiftUI.EnvironmentKey
