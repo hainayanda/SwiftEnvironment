1420a3feaea6a95659121ff1eded3f1a2b82547a
Nayanda Haberty <hainayanda@outlook.com>
2024-04-21 21:42:29 +0700

    Refactor to satisfy SwiftLint

--- Sources/SwiftEnvironmentMacro/Extensions/AttributeSyntax+Extensions.swift
+++ Sources/SwiftEnvironmentMacro/Extensions/AttributeSyntax+Extensions.swift
@@ -2,41 +2,24 @@
 //  File.swift
 //  
 //
-//  Created by Nayanda Haberty on 17/3/24.
+//  Created by Nayanda Haberty on 21/4/24.
 //
 
 import Foundation
 import SwiftSyntax
 
-let baseDefaultValues: [String: String] = [
-    "Int": "0", "Int8": "0", "Int16": "0", "Int32": "0", "Int64": "0",
-    "UInt": "0", "UInt8": "0", "UInt16": "0", "UInt32": "0", "UInt64": "0",
-    "Float": "0", "Float16": "0", "Float32": "0", "Float64": "0.0", "Double": "0.0",
-    "Bool": "false", "Character": "Character(\"\")", "String": "\"\"", "Data": "Data()",
-    "Date": "Date()", "UUID": "UUID()",
-    "CGFloat": "0", "CGSize": ".zero", "CGPoint": ".zero", "CGRect": ".zero",
-    "NSNumber": "0", "NSString": "\"\"", "NSNull": "NSNull()", "NSObject": "NSObject()",
-    "NSData": "NSData()", "NSDate": "NSDate()", "NSSet": "NSSet()", "NSArray": "NSArray()",
-    "NSSize": ".zero", "NSPoint": ".zero", "NSRect": ".zero",
-    "AnyView": "AnyView(EmptyView())"
-]
-
 extension AttributeSyntax {
     var hasModifiers: Bool {
-        get {
-            guard let firstArgument = arguments?.as(LabeledExprListSyntax.self)?
-                .first?.as(LabeledExprSyntax.self) else { return false }
-            return firstArgument.trimmedDescription == ".publicStub"
-            || firstArgument.trimmedDescription == ".internalStub"
-        }
+        guard let firstArgument = arguments?.as(LabeledExprListSyntax.self)?
+            .first?.as(LabeledExprSyntax.self) else { return false }
+        return firstArgument.trimmedDescription == ".publicStub"
+        || firstArgument.trimmedDescription == ".internalStub"
     }
     
     var isPublicStub: Bool {
-        get {
-            guard let firstArgument = arguments?.as(LabeledExprListSyntax.self)?
-                .first?.as(LabeledExprSyntax.self) else { return false }
-            return firstArgument.trimmedDescription == ".publicStub"
-        }
+        guard let firstArgument = arguments?.as(LabeledExprListSyntax.self)?
+            .first?.as(LabeledExprSyntax.self) else { return false }
+        return firstArgument.trimmedDescription == ".publicStub"
     }
     
     var defaultValueArguments: [String: String] {
@@ -68,7 +51,7 @@ private extension Sequence where Element == FunctionCallExprSyntax {
         try reduce(into: [:]) { partialResult, expression in
             let innerArgs = expression.arguments.map { $0 }
             guard innerArgs.count == 2,
-                    innerArgs[0].label?.trimmedDescription == "for",
+                  innerArgs[0].label?.trimmedDescription == "for",
                   innerArgs[1].label == nil,
                   let memberAccess = innerArgs[0].expression.as(MemberAccessExprSyntax.self),
                   let type = memberAccess.base?.trimmedDescription else {
