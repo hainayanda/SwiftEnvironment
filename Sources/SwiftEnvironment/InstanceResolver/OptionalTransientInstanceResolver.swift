8d29a0500e1e23c2c917e604e192ab08152d7c1f
Nayanda Haberty <hainayanda@outlook.com>
2025-06-01 14:20:50 +0700

    Refactor

--- Sources/SwiftEnvironment/InstanceResolver/OptionalTransientInstanceResolver.swift
+++ Sources/SwiftEnvironment/InstanceResolver/OptionalTransientInstanceResolver.swift
@@ -1,20 +1,20 @@
 //
-//  TransientInstanceResolver.swift
+//  OptionalTransientInstanceResolver.swift
 //  SwiftEnvironment
 //
-//  Created by Nayanda Haberty on 5/11/24.
+//  Created by Nayanda Haberty on 01/06/25.
 //
 
 import Foundation
 import Chary
 import SwiftUI
 
-struct TransientInstanceResolver<Value>: InstanceResolver {
+struct OptionalTransientInstanceResolver<Value>: InstanceResolver {
     let id: UUID = UUID()
-    private let resolver: () -> Value
+    private let resolver: () -> Value?
     private let queue: DispatchQueue?
     
-    @inlinable init(queue: DispatchQueue?, resolver: @escaping () -> Value) {
+    @inlinable init(queue: DispatchQueue?, resolver: @escaping () -> Value?) {
         self.resolver = resolver
         self.queue = queue
     }
